2cebb9486cc07a87da71297c24d87e69e88246d0
Hannah Jeror <jerorhn@yahoo.com>
2018-01-30 18:15:00 -0500

    Chpt19 - Add button functionality

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -7,7 +7,7 @@
             <span class="song-number">${index + 1}</span>
             <span class="ion-play"></span>
             <span class="ion-pause"></span>
-          </button>  
+          </button>
         </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
@@ -15,6 +15,7 @@
 
       song.element.on('click', event => {
         player.playPause(song);
+        $('button#play-pause').attr('playState', player.playState)
       });
 
       $('#song-list').append(song.element);
